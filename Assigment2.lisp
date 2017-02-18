52c72deaded8a81682d3a6ddcc0d7a9538ca74ed
rfiggs <rfiggs@hawaii.edu>
2017-02-17 18:26:50 -1000

    depth-first works

--- Assigment2.lisp
+++ Assigment2.lisp
@@ -45,7 +45,9 @@
 (defun make-children (parent-state moves)
     (mapcar
         (lambda (x) (list x parent-state))
-        (car (mapcar moves (list parent-state)))
+        ;(remove nil (car (mapcar moves (list parent-state))))
+        (remove nil (eval (list moves parent-state)))
+
     )
 )
 
@@ -92,6 +94,7 @@
     )
 )
 
+
 ;Function: breadth-first
 ;Description: sets up the open and closed lists then calls
 ;the recursive function breadth-first-search to find a path to the goal state
@@ -110,7 +113,66 @@
         )
         ;something went wrong creating the variables
         (T
-            (and (write 'somethingwrong))
+            nil
+        )
+    )
+)
+
+;Function: depth-first-search
+;Description: uses the global *open* and *closed* lists
+;to find a path to the goal from the start
+;Parameters:
+;goal, the target that state we are trying to achieve
+;iteration, the current number of nodes looked at
+;moves, a function that takes a state as a parameter and  returns the child states
+(defun depth-first-search (goal iteration moves)
+    (cond
+        ((> 0 (length *open*))
+            nil
+        )
+        ((print-node (car *open*)))
+        ((member (top-state *open*) (mapcar #'car *closed*))
+            (and
+                (setf *open* (cdr *open*))
+                (breadth-first-search goal (+ 1 iteration) moves)
+            )
+        )
+        ((equal goal (top-state *open*))
+            (car *open*)
+        )
+        (T
+            (and
+                (setf *closed* (cons (car *open*) *closed*))
+                (setf *open* (append
+                    (make-children (top-state *open*) moves)
+                    (cdr *open*)
+                ))
+                (breadth-first-search goal (+ 1 iteration) moves)
+            )
+        )
+    )
+)
+
+
+;Function: depth-first
+;Description: sets up the open and closed lists then calls
+;the recursive function depth-first-search to find a path to the goal state
+;Parameters:
+;start, a list containing the start state
+;goal, a list containing the goal state
+;moves, a function that takes a state and returns its valid children.
+;Returns:
+;the path from the start state to goal if it exists, otherwise nil
+(defun depth-first (start goal moves)
+    (cond
+        ;if the global variables are correctly made then call
+        ;depth-first-search which is the recursive function that finds the path
+        ((make-globals start)
+            (depth-first-search goal 0 moves)
+        )
+        ;something went wrong creating the variables
+        (T
+            nil
         )
     )
 )
